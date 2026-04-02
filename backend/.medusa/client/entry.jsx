58964a221387f9c260f475a41a0b1b5985c3f27d
martin-harley <mharley007@gmail.com>
2026-04-02 10:44:14 -0700

    Update .gitignore to exclude backend/.medusa directory

---
+++ backend/.medusa/client/entry.jsx
@@ -0,0 +1,25 @@
+import App from "@medusajs/dashboard";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import plugin0 from "@medusajs/draft-order/admin"
+import plugin1 from "@rsc-labs/medusa-booking-system/admin"
+
+let root = null
+
+if (!root) {
+  root = ReactDOM.createRoot(document.getElementById("medusa"))
+}
+
+
+root.render(
+  <React.StrictMode>
+    <App plugins={[plugin0, plugin1]} />
+  </React.StrictMode>
+)
+
+
+if (import.meta.hot) {
+    import.meta.hot.accept()
+}
